171440687dc012c81517fe1e60120af9ea2fda10
Anna Gajewska <gajewska.anna@o2.pl>
2018-03-12 18:38:15 +0100

    Change scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
 function getTriangleArea(a, h) {
 	if ((a <= 0) || (h <= 0)) {
 		return 'Nieprawidłowe dane';
-	} else if ((a > 0) || (h > 0)) {
-		return a*h/2;
-	}
+	} 
+	return a*h/2;
 }
 
 console.log(getTriangleArea(10, 6))
  
 var triangle1Area = getTriangleArea(10, 15),
-	triangle2Area = getTriangleArea(15, 10),
-	triangle3Area = getTriangleArea(5, 10);
+	triangle2Area = getTriangleArea(15, -5),
+	triangle3Area = getTriangleArea(-5, 10);
 
 console.log(triangle1Area);
 console.log(triangle2Area);
